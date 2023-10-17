116586e73cc410064c789175d2844787d7025d81
ckbb-4564 <thanadon-boom@outlook.co.th>
2023-10-17 11:13:44 +0700

    add grindspots

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+grindspots = {
+    1: {
+        "name": "Thornwood Forest",
+        "type": "Kamasylvia"
+    },
+    2: {
+        "name": "Tunkuta",
+        "type": "Kamasylvia"
+    },
+    3: {
+        "name": "Olun Valley",
+        "type": "Kamasylvia"
+    },
+    4: {
+        "name": "Crypt of Resting's Thought",
+        "type": "Kamasylvia"
+    }
+}
+
 @app.get("/")
 def home():
     return {"message": "home"}
+
+@app.get("/grindspots")
+def get_grindspots():
+    return grindspots
+
+@app.get("/grindspot/{id}")
+def get_grindspot(id: int):
+    return grindspots[id]
